6f1fb32eb81615916bd2a9cb24037c8bd924824d
martin byrne <marb61a@gmail.com>
2017-07-25 16:45:07 +0000

    added unit tests

--- src/lib/todoHelpers.test.js
+++ src/lib/todoHelpers.test.js
@@ -41,3 +41,34 @@ test('addTodo should not mutate the existing todo array', () => {
     
     expect(result).not.toBe(startTodos);
 });
+
+//   In the Jest testing framework you are able to skip tests
+//   Add .skip after the test keyword in the helpersSpec file 
+
+test('findById should return the expected item from an array', () => {
+    const startTodos =[
+        {id: 1, name: 'one', isComplete: false},
+        {id: 2, name: 'two', isComplete: false},
+        {id: 3, name: 'three', isComplete: false}
+    ]; 
+    
+    const expected = {id: 2, name: 'two', isComplete: false};
+    const result = findById(2, startTodos);
+    expect(result).toEqual(expected);
+});
+
+test('toggleTodo should toggle the isComplete prop of a todo', () => {
+    
+});
+
+test('toggleTodo should not mutate the original todo', () => {
+    
+});
+
+test('updateTodo should update an item by id', () => {
+    
+});
+
+test('updateTodo should not mutate the original array', () => {
+    
+});
