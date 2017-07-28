b6d81dccd0b8fa6293c2579d24c095b9fe801c7d
martin byrne <marb61a@gmail.com>
2017-07-28 17:07:44 +0000

    added utility file  tests for pipe utility

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,1 +1,7 @@
-export const partial = (fn, ...args) => fn.bind(null, ...args); 
+// The first ...args is a rest operator and the second is a spread operator
+export const partial = (fn, ...args) => fn.bind(null, ...args);
+
+export const pipe = (f, g) => (...args) => {
+    
+};
+
