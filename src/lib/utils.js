bfd40ebcaafa06e1bb054ed39542d52807674b22
martin byrne <marb61a@gmail.com>
2017-08-02 16:50:52 +0000

    added removeTodo tests and helper function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,4 +3,5 @@ export const partial = (fn, ...args) => fn.bind(null, ...args);
 
 const _pipe = (f, g) => (...args) => g(f(...args));
 
+// Takes in the old pipe function as it will allow more than 2 functions
 export const pipe = (...fns) => fns.reduce(_pipe);
