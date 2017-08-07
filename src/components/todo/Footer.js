a620d7faa547225e38e0e3de6b1ee799be05f0b0
martin byrne <marb61a@gmail.com>
2017-08-07 16:37:36 +0000

    added initial footer component

---
+++ src/components/todo/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from '../router';
+
+export const Footer = () => {
+    return(
+        <div>
+            <link>All</link>
+            <link>Active</link>
+            <link>Complete</link>
+        </div> 
+    );  
+};
