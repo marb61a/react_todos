88d1e984be42814382ef82a23b6c89091b0c4843
martin byrne <marb61a@gmail.com>
2017-08-16 16:59:47 +0000

    added filtertodo functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {TodoForm, TodoList, Footer} from './components/todo';
-import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo} from './lib/todoHelpers';
+import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './lib/todoHelpers';
 import {pipe, partial } from './lib/utils';
 
 
@@ -15,6 +15,11 @@ class App extends Component {
       ],
       currentTodo: ''
     };
+    
+  static contextTypes = {
+    route: React.PropTypes.string
+    
+  }
     
   handleRemove = (id, evt) => {
     evt.preventDefault();
@@ -63,6 +68,8 @@ class App extends Component {
   render() {
     // Returns a submit if true or an empty submit and error message if false
     const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit;
+    const displayTodos = filterTodos(this.state.todos, this.context.route);
+    
     return (
       <div className="App">
         <div className="App-header">
@@ -80,7 +87,7 @@ class App extends Component {
             handleSubmit={submitHandler}/>
           <TodoList 
             handleToggle={this.handleToggle} 
-            todos={this.state.todos}
+            todos={displayTodos}
             handleRemove={this.handleRemove}
           />
           <Footer />
