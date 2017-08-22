f8a5eda1bfeb016b9f17f2bf89e09be35ac8f60b
martin byrne <marb61a@gmail.com>
2017-08-22 16:06:51 +0000

    added todo toggle feature

--- src/lib/todoService.js
+++ src/lib/todoService.js
@@ -18,3 +18,16 @@ export const createTodo = (todo) => {
     }).then(res => res.json);
     
 };
+
+export const saveTodo = (todo) => {
+    return fetch(`${baseUrl}/${todo.id}`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+        
+    }).then(res => res.json);
+    
+};
